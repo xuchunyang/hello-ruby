83889858c39ed60a18460c0dab6a33bae266f8a1
Chunyang Xu <xuchunyang.me@gmail.com>
2016-04-15 00:25:23 +0800

    count-commands-in-path.rb: Align output

--- count-commands-in-path.rb
+++ count-commands-in-path.rb
@@ -13,14 +13,27 @@ def count_commands(dir)
   end
 end
 
+def longest(array)
+  longest = 0
+  array.each do |str|
+    if longest < str.length
+      longest = str.length
+    end
+  end
+
+  return longest
+end
+
+vaild_paths = ENV['PATH'].split(':').delete_if { |dir| !Dir.exist?(dir) }
+
+length_of_longest_path = longest(vaild_paths)
+
 total_count = 0
 
-ENV["PATH"].split(":").each do |dir|
-  if Dir.exist?(dir)
-    count = count_commands(dir)
-    total_count += count
-    puts "#{dir}  --  #{count}"
-  end
+vaild_paths.each do |dir|
+  count = count_commands(dir)
+  total_count += count
+  printf "%-#{length_of_longest_path}s\t#{count}\n", dir
 end
 
 puts "\nYou have #{total_count} commands"
