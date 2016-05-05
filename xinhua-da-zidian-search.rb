ae7624162db98e5dd5a9b6771f26d7ff6c752e1d
Chunyang Xu <xuchunyang.me@gmail.com>
2016-05-05 16:46:26 +0800

    xinhua-da-zidian-search: Search multiple word at once

--- xinhua-da-zidian-search.rb
+++ xinhua-da-zidian-search.rb
@@ -4,15 +4,17 @@ require 'chinese_pinyin'
 
 abort("Missing Argument") unless ARGV[0]
 
-pinyin = Pinyin.t(ARGV[0])
+pinyin = Pinyin.t(ARGV[0]).split(' ')
 
 DATA_FILE = File.expand_path("Projects/hello-ruby/xinhua-da-zidian-pinyin.txt", "~")
 
 File.open(DATA_FILE) do |file|
   file.each_line do |line|
-    if line.split[0] == pinyin
+    break if pinyin.empty?
+    aux = line.split[0]
+    if pinyin.include?(aux)
+      pinyin.delete(aux)
       puts line
-      break
     end
   end
 end
